057257aa2f82b7413e86ff4e3630c2926ad8a9fa
Jørund Vier Skriubakken <jvs@enonic.com>
2014-01-22 19:02:26 +0100

    CMS-2845 Rendering - Provide access to page data when content is created landing-page now uses provided context "pageRegions" instead of  "content.page.regions" - otherwise no regions will be available when creating a new page

--- modules/demo-1.0.0/component/landing-page/get.js
+++ modules/demo-1.0.0/component/landing-page/get.js
@@ -1,13 +1,16 @@
 var editMode = portal.request.mode == 'edit';
 
 var content = portal.content;
+var pageRegions = portal.pageRegions;
 var body = system.mustache.render('views/frogger.html', {
     title: content.displayName,
     path: content.path,
     name: content.name,
     editable: editMode,
     resourcesPath: portal.url.createResourceUrl(''),
-    mainRegion: content.page.regions.getRegion("main")
+    mainRegion: pageRegions.getRegion("main"),
+    leftRegion: pageRegions.getRegion("left"),
+    rightRegion: pageRegions.getRegion("right")
 });
 
 portal.response.body = body;
