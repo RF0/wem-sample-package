f81d519d82b5af205d321fd9c1bff71493159f0a
Sten Roger Sandvik <srs@enonic.com>
2014-01-30 14:35:20 +0100

    Added natural template module

---
+++ modules/natural-1.0.0/service/test/get.js
@@ -0,0 +1,10 @@
+
+var params = {
+	title: 'Hello Dynamic :-)'
+};
+
+var body = system.thymeleaf.render('views/sample.html', params);
+
+portal.response.contentType = 'text/html';
+portal.response.body = body;
+
