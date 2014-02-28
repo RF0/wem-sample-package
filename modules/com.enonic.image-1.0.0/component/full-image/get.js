7a4953ab296bd8df7fb235aa13c857342f122245
Alex Rodriguez <aro@enonic.com>
2014-02-28 13:58:32 +0100

    Made full-image component render the image.

--- modules/com.enonic.image-1.0.0/component/full-image/get.js
+++ modules/com.enonic.image-1.0.0/component/full-image/get.js
@@ -1,5 +1,12 @@
-var image = portal.component;
+var component = portal.component;
+var componentPath = component.path;
+var componentType = component.type;
+var imageUrl = component != null ? portal.url.createImageByIdUrl(component.image) : '';
 
-portal.response.body = 'Image component: ' + image.name;
+var html = '<div data-live-edit-type="'+componentType+'" data-live-edit-component="'+componentPath+'">' +
+               '<img style="border-radius:50px;" src="'+imageUrl+'"/>' +
+           '</div>';
+
+portal.response.body = html;
 portal.response.contentType = 'text/html';
 portal.response.status = 200;
