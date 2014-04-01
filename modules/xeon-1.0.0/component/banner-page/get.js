68a4762da6cbdad50acbce776eb7b74040db83df
Lasse Jul-Larsen <ljl@enonic.com>
2014-04-01 09:48:28 +0200

    CMS-3270 Sample site - Create new page template with banner support

--- modules/xeon-1.0.0/component/banner-page/get.js
+++ modules/xeon-1.0.0/component/banner-page/get.js
@@ -1,16 +1,18 @@
 var content = portal.content;
+var page = portal.content.page;
 var pageRegions = portal.pageRegions;
 var contents = system.contentService.getRootContent();
 var editMode = portal.request.mode == 'edit';
+var slides = page.config.dataSets("slide");
 
 var params = {
 	context: portal,
 	pageRegions: pageRegions,
-	topRegion: pageRegions.getRegion("top"),
 	mainRegion: pageRegions.getRegion("main"),
 	contents: contents,
 	editable: editMode,
-	banner: true
+	banner: true,
+	slides: slides
 };
 
 var body = system.thymeleaf.render('views/page.html', params);
