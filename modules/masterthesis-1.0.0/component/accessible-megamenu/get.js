0631c1aecb66bc0eda5252e56a862cd0b57da005
Rune Forberg <rfo@enonic.com>
2014-05-29 20:56:01 +0200

    Add rending of menu item content

--- modules/masterthesis-1.0.0/component/accessible-megamenu/get.js
+++ modules/masterthesis-1.0.0/component/accessible-megamenu/get.js
@@ -1,14 +1,19 @@
 var component = portal.component;
 var site = portal.siteContent;
 var content = portal.content;
+var contents = system.contentService.getChildContent(site.path+"/megamenu").getList().toArray();
 var editMode = portal.request.mode == 'edit';
-
+var article,articleData;
+var contentId;
 
 var params = {
     context: portal,
     component: component,
     menuContents: getMegaMenuContents(),
-    title: "Master Thesis Menu"
+    article: article,
+    articleData: articleData,
+    title: "Master Thesis Menu",
+    contentId: contentId
 };
 
 var body = system.thymeleaf.render('view/accessible-megamenu/index.html', params);
@@ -17,6 +22,11 @@ portal.response.contentType = 'text/html';
 portal.response.body = body;
 
 function getMegaMenuContents(){
-    var contents = system.contentService.getChildContent(site.path+"/megamenu").getList().toArray();
+
+    contentId = contents[0].id;
+    article = system.contentService.getContentById(contentId);
+    articleData = {
+        preface:article.contentData.getProperty("preface").value
+    }
     return contents;
 }
